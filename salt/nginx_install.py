a884d3fb496a31dd2de6cdec3a7ba593507f0a36
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-04-28 15:36:14 +0300

    Add files via upload

---
+++ salt/nginx_install.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import subprocess
+
+if len(sys.argv) != 2:
+    print("Неверное количество аргументов")
+    print("Пример: python nginx_install.py <clientID>")
+    sys.exit(1)
+
+clientID = sys.argv[1]
+print("Начинаю установку Nginx для "+clientID)
+print("Внимание, прцоесс установки может занять некоторое время...")
+process = subprocess.Popen(['sudo', 'salt', clientID, 'state.apply', 'nginx_install'],
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+out = stdout.decode("utf-8")
+if stderr:
+    print("Ошибка установки "+clientID)
+    err = stderr.decode("utf-8")
+    print("Логи запишутся в файл /var/log/salt/transfer_file.log")
+    with open("/var/log/salt/transfer_file.log", "w") as file:
+        file.write(err)
+        file.write(out)
+    print(err)
+    print(out)
+    sys.exit(1)
+print("Перенос установка Nginx для "+clientID+" успешно завершена!")
+sys.exit(0)
---
+++ salt/transfer_file.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import subprocess
+
+if len(sys.argv) != 2:
+    print("Неверное количество аргументов")
+    print("Пример: python transfer_file.py <clientID>")
+    sys.exit(1)
+
+clientID = sys.argv[1]
+print("Начинаю перенос файлов для клиента "+clientID)
+process = subprocess.Popen(['sudo', 'salt', clientID, 'state.apply', 'nginx_files_send'],
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+out = stdout.decode("utf-8")
+if stderr:
+    print("Ошибка переноса файлов для клиентов "+clientID)
+    err = stderr.decode("utf-8")
+    print("Логи запишутся в файл /var/log/salt/transfer_file.log")
+    with open("/var/log/salt/transfer_file.log", "w") as file:
+        file.write(err)
+        file.write(out)
+    print(err)
+    print(out)
+    sys.exit(1)
+print("Перенос файлов для клиентов "+clientID+" завершен")
+sys.exit(0)
